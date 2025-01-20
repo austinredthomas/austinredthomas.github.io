3fcdf8c88aca4af3eda7f1572cc58fedfbaec3d9
austinredthomas <austinredthomas@gmail.com>
2025-01-20 17:12:58 -0500

    Changing location of components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import '@root/global.scss';
+
+import * as Constants from '@common/constants';
+import * as Utilities from '@common/utilities';
+
+// NOTE(jimmylee): This is a kitchen sink of all components.
+// When forking or remixing, you'll likely only need a few.
+import ActionBar from '@components/ActionBar';
+
+export const dynamic = 'force-static';
 
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to My React App!</h1>
+      <h1>What is up dudes!</h1>
       <p>This is the beginning of something awesome.</p>
     </div>
   );
